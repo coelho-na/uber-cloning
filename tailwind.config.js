1eaedba84916e60830515ef7f7aa3300da8ec846
coelho-na <nadinecoelho9@gmail.com>
2024-04-23 20:02:54 -0300

    finish hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'uber': "url('/src/assets/img/bguber.png')",
+        'city': "url('/src/assets/img/bg-city.png')",
+      }
+    },
   },
   plugins: [],
 }
